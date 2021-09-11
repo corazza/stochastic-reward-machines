5154fb92fa6cfc0a6ac214841cb4f6c5f2d06d79
Jan Corazza <corazzajan@gmail.com>
2021-09-11 11:39:01 +0200

    moving around

--- scripts/env.sh
+++ scripts/env.sh
@@ -2,7 +2,7 @@
 today=`date '+%Y_%m_%d__%H_%M_%S'`
 hourly=`date '+%Y_%m_%d__%H'`
 exp_name=${1:-${hourly}}
-outer_results="../mpi_results/${exp_name}"
+outer_results="../results/${exp_name}"
 results_dir="${outer_results}/${envalg_name}_${today}"
 
 mkdir -p $outer_results
