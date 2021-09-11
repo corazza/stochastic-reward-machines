20959f2071313565938c2ba995eb31d3ae5a9144
Jan Corazza <corazzajan@gmail.com>
2021-09-11 15:41:33 +0200

    scripts, readme

---
+++ scripts/run_mining_sequential.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+exp_name="mining"
+. ./env.sh ${1}
+
+cd ../src
+for i in `seq 1 10`; 
+do
+	python run.py --alg=${1} --seed=${i} --env=MiningT3-v0 --print_freq=100000 --num_timesteps=3e6 --gamma=0.9 --results_path=${results_dir}/MiningT3-v0-${i}.json --rm_hidden
+done
